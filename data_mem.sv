c840ea700d1e0020f9dc30bb46fdd66c8b034c0d
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2026-05-11 19:28:26 +0530

    data memory added

---
+++ data_mem.sv
@@ -0,0 +1,21 @@
+module data_mem (
+	input logic clk,
+	input logic mem_read,
+	input logic mem_write,
+	input logic [31:0] addr,
+	input logic [31:0] write_data,
+	output logic [31:0] read_data
+);
+
+logic [31:0] data_mem [0:255];
+
+always_ff (posedge clk) begin
+	if (mem_write)
+		data_mem[addr[9:2]] <= write_data;
+end
+
+assign read_data = (mem_read) ? data_mem[addr[9:0]] : 32'd0;
+
+
+
+endmodule
